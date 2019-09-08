9e5aaa2770c6fe2b6cee84647f3f0d9c65b24665
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-08 15:46:18 +0200

    Src | accepts 'string', 'number' and 'symbol' as types of keys in EmitterInstance, thus window will be valid emitterInstance

--- src/_types.ts
+++ src/_types.ts
@@ -4,18 +4,32 @@ export type EventCallback = (event: Event) => void;
 
 export type EventName = string;
 export type EventData = [EventCallback, Subscriber[]];
-export type EmitterInstance = Record<string, unknown>;
+export type EmitterInstance = Record<string | number | symbol, unknown>;
 
 export type SubscriptionFunctions = (eventName: string, eventCallback: EventCallback) => void;
 export type PublisherProps = [ EmitterInstance, string, string ];
 
 export const isValidEmitter = (
-    emitterInstance: Record<string, unknown>,
-    addListenerMethodName: string,
-    removeListenerMethodName: string
-): emitterInstance is EmitterInstance => {
+  emitterInstance: EmitterInstance,
+  addListenerMethodName: string | number,
+  removeListenerMethodName: string | number
+  ): emitterInstance is EmitterInstance => {
+
+  const isOfType = (value: unknown, type: 'symbol' | 'function'): boolean => typeof value === type;
+
+  const addMethodName: keyof typeof emitterInstance = addListenerMethodName;
+  const removeMethodName: keyof typeof emitterInstance = removeListenerMethodName;
+
+  const addKey = isOfType(addListenerMethodName, 'symbol')
+    ? addListenerMethodName
+    : addMethodName;
+
+  const removeKey = isOfType(removeListenerMethodName, 'symbol')
+    ? removeListenerMethodName
+    : removeMethodName;
+
   return (
-    typeof emitterInstance[ addListenerMethodName ] === 'function' &&
-    typeof emitterInstance[ removeListenerMethodName ] === 'function'
+    isOfType(emitterInstance[ addKey ], 'function') &&
+    isOfType(emitterInstance[ removeKey ], 'function')
   );
 };
