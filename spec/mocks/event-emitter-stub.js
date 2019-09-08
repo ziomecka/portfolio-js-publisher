dadd552273acff9e748016dda8efabe5c9cba7f3
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-08 07:55:58 +0200

    Src | removes subscription to events in event emitter stub

--- spec/mocks/event-emitter-stub.js
+++ spec/mocks/event-emitter-stub.js
@@ -1,14 +1,4 @@
 const { EventEmitter } = require('events');
 
-class EventEmitterStub extends EventEmitter {
-  constructor () {
-    super();
-    this.on('fooEvent', () => console.log('foo event occurred!'));
-    this.on('barEvent', () => console.log('bar event occurred!'));
-  }
-}
-
-const eventEmitterStub = new EventEmitterStub();
-
-module.exports = eventEmitterStub;
+module.exports = new EventEmitter();
 
